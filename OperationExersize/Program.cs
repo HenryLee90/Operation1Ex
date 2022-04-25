7a1b9a42d30c655a66068addc33397dad9030a8e
Henry Lee <hlee09151@gmail.com>
2022-04-25 18:49:39 -0500

    Third Commit

--- OperationExersize/Program.cs
+++ OperationExersize/Program.cs
@@ -6,38 +6,38 @@ namespace OperationExersize
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int x = 4;
-            int y = 7;
-            int answer = (x + y);
-            Console.WriteLine(answer);
+           Console.WriteLine("Hello World!");
+           int x = 4;
+           int y = 7;
+           int answer = (x + y);
+           Console.WriteLine(answer);
+           
+           int x1 = 12;
+           int y1 = 6;
+           int answer1 = (x1 - y1);
+           Console.WriteLine(answer1);
+           
+           int x2 = 76;
+           int y2 = 59;
+           int answer2 = (x2 * y2);
+           Console.WriteLine(answer2);
 
-            int x1 = 12;
-            int y1 = 6;
-            int answer1 = (x1 - y1);
-            Console.WriteLine(answer1);
+           int a = 57;
+           int b = 5;
+           int quoitent = (a / b);
+           int remainder = (a % b);
+           Console.WriteLine(quoitent);
+           Console.WriteLine(remainder);
+           Console.WriteLine($" {a} / {b} is  {quoitent}");
+           Console.WriteLine($"the remainder of {a} % {b} is remainder of {remainder}");
 
-            int x2 = 76;
-            int y2 = 59;
-            int answer2 = (x2 * y2);
-            Console.WriteLine(answer2);
-
-            int a = 57;
-            int b = 5;
-            int quoitent = (a / b);
-            int remainder = (a % b);
-            Console.WriteLine(quoitent);
-            Console.WriteLine(remainder);
-            Console.WriteLine($" {a} / {b} is  {quoitent}");
-            Console.WriteLine($"the remainder of {a} % {b} is remainder of {remainder}");
-
-
+            Something();
 
 
 
         }
 
-        private static double AreaOfCircle(double r)
+        public static double AreaOfCircle(double r)
         {
 
             return Math.PI * Math.Pow(r, 2);
@@ -45,13 +45,14 @@ namespace OperationExersize
 
         }
 
-        private static void Something()
+        public static void Something()
         {
-
+            
             Console.WriteLine("Please enter the radius: ");
             double radius = double.Parse(Console.ReadLine());
             double area = AreaOfCircle(radius);
             Console.WriteLine(area);
+          
 
         }
     }
